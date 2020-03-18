10358bd9074083553228cf5f9822911f9be610d2
shengxinjing <316783812@qq.com>
2020-03-18 17:51:38 +0800

    cancel push

--- bili.py
+++ bili.py
@@ -5,11 +5,7 @@ import time
 url = 'https://oapi.dingtalk.com/robot/send?access_token=dfb282779fa574887abaade18616db9be415fcc4b11289a6bd487a59f2b5a641'
 
 bili_ids = ['37663924','437316738']
-# https://api.bilibili.com/x/space/arc/search?mid=37663924&pn=1&ps=25&jsonp=jsonp
-# bilibili_url = ''
-
-# print(time.time())
-yesterday = time.time()-60*60*24*1
+yesterday = time.time()-60*60*24*1  # 1天前
 for bid in bili_ids:
     bili_url = 'https://api.bilibili.com/x/space/arc/search?mid='+bid+'&pn=1&ps=25&jsonp=jsonp'
 
